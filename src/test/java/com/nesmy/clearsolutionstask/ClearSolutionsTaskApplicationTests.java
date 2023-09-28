ef582182ee35d0a60baee215c747891d3aa22e9c
Nesmy <anastasiia.mudra.kn.2021@lpnu.ua>
2023-09-28 17:12:42 +0300

    Initial commit

---
+++ src/test/java/com/nesmy/clearsolutionstask/ClearSolutionsTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nesmy.clearsolutionstask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClearSolutionsTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
